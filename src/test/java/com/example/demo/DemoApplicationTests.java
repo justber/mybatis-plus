9b57abf4cfbdd4ce560a35418a70c3484452325e
何超 <1444298847@qq.com>
2022-03-31 22:07:18 +0800

    测试mybatis-plus的基本crud

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -25,4 +25,39 @@ class DemoApplicationTests {
 		users.forEach(System.out::println);
 	}
 
+
+	/**
+	 * 测试添加用户
+	 */
+	@Test
+	public void testAddUser() {
+		User user = new User();
+
+		user.setName("Helen");
+		user.setAge(18);
+		user.setEmail("55317332@qq.com");
+
+		int insert = userMapper.insert(user);
+		System.out.printf("**************************************************************");
+		System.out.println(insert); // 打印更新影响了几行
+		System.out.printf("**************************************************************");
+		System.out.println(user); // 回传数据
+	}
+
+	/**
+	 * 测试修改用户
+	 */
+	@Test
+	public void testUpdateuser(){
+		User user = new User();
+		user.setId(2);
+		user.setAge(34);
+
+		int i = userMapper.updateById(user);
+		System.out.println("--------------------------------------------");
+		System.out.println(i); // 对多少条数据产生了影响
+		System.out.println("--------------------------------------------");
+		System.out.println(user);
+	}
+
 }
