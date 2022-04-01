6970e40eac5b0a66f812e95c4908369948921a4d
何超 <1444298847@qq.com>
2022-04-01 22:08:35 +0800

    提交到dev分支测试

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,8 +1,13 @@
 package com.example.demo.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.Version;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -12,4 +17,13 @@ public class User {
     private String name;
     private int age;
     private String email;
+
+    @TableField(fill = FieldFill.INSERT) // 标注该字段为非主键的字段标识，insert表示插入填充字段
+    private Date createTime;
+    @TableField(fill = FieldFill.UPDATE) // 表示插入并更新该字段
+    private Date update_time;
+
+    @Version
+    @TableField(value = "version")
+    private int version;
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
@@ -7,10 +9,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.util.*;
 
 @SpringBootTest
-@MapperScan("com.example.demo.mapper")
+//@MapperScan("com.example.demo.mapper")
 class DemoApplicationTests {
 
 	@Autowired
@@ -33,9 +35,9 @@ class DemoApplicationTests {
 	public void testAddUser() {
 		User user = new User();
 
-		user.setName("Helen");
-		user.setAge(18);
-		user.setEmail("55317332@qq.com");
+		user.setName("wangwu");
+		user.setAge(31);
+		user.setEmail("djshdjhadh@qq.com");
 
 		int insert = userMapper.insert(user);
 		System.out.printf("**************************************************************");
@@ -50,8 +52,8 @@ class DemoApplicationTests {
 	@Test
 	public void testUpdateuser(){
 		User user = new User();
-		user.setId(2);
-		user.setAge(34);
+		user.setId(9);
+		user.setAge(9);
 
 		int i = userMapper.updateById(user);
 		System.out.println("--------------------------------------------");
@@ -60,4 +62,59 @@ class DemoApplicationTests {
 		System.out.println(user);
 	}
 
+	/**
+	 * 测试添加数据
+	 */
+	@Test
+	public void testInsertUser(){
+		User user = new User();
+		user.setId(9);
+		user.setAge(3);
+		user.setName("hdad");
+		int result = userMapper.updateById(user);
+		System.out.println(result);
+	}
+
+	@Test
+	public void testOptimisticLocker() {
+		//查询
+		User user = new User();
+		//修改数据
+		user.setId(1);
+		user.setName("Helen Yao");
+		user.setEmail("helen@qq.com");
+		//执行更新
+		userMapper.updateById(user);
+	}
+
+	// 根据id查询用户
+	@Test
+	public void selectUser(){
+		User user = userMapper.selectById(1);
+		System.out.println(user);
+	}
+
+	//根据多个id，批量查询user
+	@Test
+	public void selectAllIdUser() {
+		List<User> users = userMapper.selectBatchIds(Arrays.asList(1,2,4));
+		System.out.println(users);
+
+	}
+	
+	// 测试分页
+	@Test
+	public void pageTest(){
+		Page<User> page = new Page<>(1,5);
+
+		page.getRecords().forEach(System.out::println);
+		System.out.println(page.getCurrent());
+		System.out.println(page.getPages());
+		System.out.println(page.getSize());
+		System.out.println(page.getTotal());
+		System.out.println(page.hasNext());
+		System.out.println(page.hasPrevious());
+
+
+	}
 }
